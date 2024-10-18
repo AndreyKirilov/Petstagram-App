ad11b522720d5ecd8e6c9a28266e78ba9eac573f
AndreyKirilov <145570409+AndreyKirilov@users.noreply.github.com>
2024-10-18 15:45:19 +0300

    Added Class Based Views

--- petstagram/pets/urls.py
+++ petstagram/pets/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from petstagram.pets import views
 
 urlpatterns = [
-    path('add/', views.pet_add_page, name='add-pet'),
+    path('add/', views.AddPetView.as_view(), name='add-pet'),
     path('<str:username>/pet/<slug:pet_slug>/', include([
-        path('', views.pet_details_page, name='pet-details'),
-        path('edit/', views.pet_edit_page, name='edit-pet'),
-        path('delete/', views.pet_delete_page, name='delete-pet'),
+        path('', views.PetDetailsView.as_view(), name='pet-details'),
+        path('edit/', views.EditPetView.as_view(), name='edit-pet'),
+        path('delete/', views.DeletePetView.as_view(), name='delete-pet'),
     ]))
 ]
